d579394529e6ff2f400dbbb277f6c0cbd7820723
Chris Talkington <chris@talkingtontech.com>
2020-03-09 23:29:32 -0500

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -13,9 +13,9 @@ DEFAULT_PRINTER_URI = "ipps://printer.example.com:631/ipp/print"
 
 class FakeResolver:
     _LOCAL_HOST = {
-        0: '127.0.0.1',
-        socket.AF_INET: '127.0.0.1',
-        socket.AF_INET6: '::1'
+        0: "127.0.0.1",
+        socket.AF_INET: "127.0.0.1",
+        socket.AF_INET6: "::1"
     }
 
     def __init__(self, fakes, *, loop):
@@ -48,16 +48,21 @@ class FakeIPP:
         self.handler = None
         self.server = None
         here = pathlib.Path(__file__)
-        ssl_cert = here.parent / 'server.crt'
-        ssl_key = here.parent / 'server.key'
+        ssl_cert = here.parent / "server.crt"
+        ssl_key = here.parent / "server.key"
         self.ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
         self.ssl_context.load_cert_chain(str(ssl_cert), str(ssl_key))
 
     async def start(self):
         port = unused_port()
         self.handler = self.app.make_handler()
-        self.server = await self.loop.create_server(self.handler,'127.0.0.1', port, ssl=self.ssl_context)
-        return {'printer.example.com': port}
+        self.server = await self.loop.create_server(
+            self.handler,
+            "127.0.0.1",
+            port,
+            ssl=self.ssl_context
+        )
+        return {"printer.example.com": port}
 
     async def stop(self):
         self.server.close()
