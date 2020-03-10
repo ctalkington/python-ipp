b2d4ec2e672bf308a1ae1a37df01e33491316b6f
Chris Talkington <chris@talkingtontech.com>
2020-03-09 22:22:59 -0500

    Update test_printer.py

--- tests/test_printer.py
+++ tests/test_printer.py
@@ -1,8 +1,25 @@
 """Tests for Printer."""
+import aiohttp
 import pytest
 
 from ipp import IPP, Printer
-from tests import DEFAULT_PRINTER_URI
+from tests import DEFAULT_PRINTER_URI, FakeIPPServer, FakeResolver
+
+@pytest.fixture()
+async def info(event_loop):
+    fake_ipp = FakeIPPServer(loop=event_loop)
+    info = await fake_ipp.start()
+    yield info
+    await fake_ipp.stop()
+
+
+@pytest.fixture()
+async def session(event_loop, info):
+    resolver = FakeResolver(info, loop=event_loop)
+    connector = aiohttp.TCPConnector(loop=event_loop, resolver=resolver, verify_ssl=False)
+    session = aiohttp.ClientSession(connector=connector, loop=event_loop)
+    yield session
+    await session.close()
 
 
 def test_init() -> None:
