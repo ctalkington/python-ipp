0ce55792b0f7f8e73d4e08692eed149611eb7057
Chris Talkington <chris@talkingtontech.com>
2020-03-09 23:43:43 -0500

    Update exceptions.py

--- ipp/exceptions.py
+++ ipp/exceptions.py
@@ -1,9 +1,11 @@
 """Exceptions for IPP."""
 class IPPError(Exception):
     """Generic IPP exception."""
+
     pass
 
 
 class IPPConnectionError(IPPError):
     """IPP connection exception."""
+
     pass
