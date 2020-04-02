44766df8ad740e48426f54371428ff967aa19576
Chris Talkington <chris@talkingtontech.com>
2020-04-02 17:07:18 -0500

    Update parser.py

--- pyipp/parser.py
+++ pyipp/parser.py
@@ -203,9 +203,9 @@ def parse_make_and_model(make_and_model: str) -> Tuple[str, str]:
         if test_against.startswith(known_make):
             found_make = True
             mlen = len(known_make)
-            mlen -= mlen
+            mlen -= 1
             make = make_and_model[:mlen]
-            mlen += mlen
+            mlen += 1
             model = make_and_model[mlen:].strip()
             break
 
