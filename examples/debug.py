f1ffda8135afd43b10e7586d6160fff77ca202bb
Chris Talkington <chris@talkingtontech.com>
2020-04-06 19:52:13 -0500

    Create debug.py

---
+++ examples/debug.py
@@ -0,0 +1,39 @@
+# pylint: disable=W0621
+"""Asynchronous Python client for IPP."""
+import asyncio
+
+from pyipp import IPP
+
+
+async def main():
+    """Show example of connecting to your IPP print server."""
+    async with IPP("ipps://EPSON761251.local:631/ipp/print") as ipp:
+        response = await ipp.execute(
+            0x000B,
+            {
+                "operation-attributes-tag": {
+                    "requested-attributes": [
+                        "printer-name",
+                        "printer-type",
+                        "printer-location",
+                        "printer-info",
+                        "printer-make-and-model",
+                        "printer-state",
+                        "printer-state-message",
+                        "printer-state-reason",
+                        "printer-uri-supported",
+                        "device-uri",
+                        "printer-is-shared",
+                    ],
+                },
+            },
+            raw=True,
+        )
+
+        with open("printer-attributes.bin", "wb") as f:
+            f.write(response)
+            f.close()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
