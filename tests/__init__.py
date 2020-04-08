41abfe28530e6bb9c60e10b93dc37fe610eb4b0d
Chris Talkington <chris@talkingtontech.com>
2020-04-07 22:31:33 -0500

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,7 +8,8 @@ DEFAULT_PRINTER_URI = (
     f"ipp://{DEFAULT_PRINTER_HOST}:{DEFAULT_PRINTER_PORT}{DEFAULT_PRINTER_PATH}"
 )
 
-IPP11_PRINTER_ATTRIBUTES = {
+# RFC 2911
+IPP11_PRINTER_ATTRS = {
     "attributes-charset": "",
     "attributes-natural-language": "",
     "charset-configured": "",
@@ -37,14 +38,80 @@ IPP11_PRINTER_ATTRIBUTES = {
     "version-number": "",
 }
 
+# PWG 5100.9
+PWG51009_PRINTER_ATTRS = {
+    "feed-orientation-default": "",
+    "feed-orientation-supported": "",
+    "job-creation-attributes-supported": "",
+    "job-ids-supported": "",
+    "job-password-supported": "",
+    "job-password-encryption-supported": "",
+    "media-col-database": "",
+    "which-jobs-supported": "",
+}
+
+# PWG 5100.11
+PWG510011_PRINTER_ATTRS = {
+    "feed-orientation-default": "",
+    "feed-orientation-supported": "",
+    "job-creation-attributes-supported": "",
+    "job-ids-supported": "",
+    "job-password-supported": "",
+    "job-password-encryption-supported": "",
+    "media-col-database": "",
+    "which-jobs-supported": "",
+}
+
+# PWG 5100.13
+PWG510013_PRINTER_ATTRS = {
+    "document-password-supported": "",
+    "identify-actions-default": "",
+    "identify-actions-supported": "",
+    "ipp-features-supported": "",
+    "job-constraints-supported": "",
+    "job-preferred-attributes-supported": "",
+    "job-resolvers-supported": "",
+    "media-bottom-margin-supported": "",
+    "media-col-database.media-source-properties": "",
+    "media-col-ready.media-source-properties": "",
+    "media-left-margin-supported": "",
+    "media-right-margin-supported": "",
+    "media-source-supported": "",
+    "media-top-margin-supported": "",
+    "multiple-operation-timeout-action": "",
+    "print-color-mode-default": "",
+    "print-color-mode-supported": "",
+    "print-rendering-intent-default": "",
+    "print-rendering-intent-supported": "",
+    "printer-charge-info": "",
+    "printer-charge-info-url": "",
+    "printer-config-change-date-time": "",
+    "printer-config-change-time": "",
+    "printer-geo-location": "",
+    "printer-get-attributes-supported": "",
+    "printer-icc-profiles": "",
+    "printer-icons": "",
+    "printer-mandatory-job-attributes": "",
+    "printer-organization": "",
+    "printer-organizational-unit": "",
+    "printer-supply": "",
+    "printer-supply-description": "",
+    "printer-supply-info-uri": "",
+    "printer-uuid": "",
+    "which-jobs-supported": "",
+}
+
+# PWG 5100.13
 IPP20_PRINTER_ATTRIBUTES = {
-    **IPP11_PRINTER_ATTRIBUTES,
+    **IPP11_PRINTER_ATTRS,
+    **PWG51009_PRINTER_ATTRS,
+    **PWG510011_PRINTER_ATTRS,
+    **PWG510013_PRINTER_ATTRS,
     "color-supported": "",
     "copies-default": "",
     "copies-supported": "",
     "finishings-default": "",
     "finishings-supported": "",
-    "job-creation-attributes-supported": "",
     "media-default": "",
     "media-ready": "",
     "media-supported": "",
