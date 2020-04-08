4a95eb7b9780ba4c24f046bdc421b97f4210f531
Chris Talkington <chris@talkingtontech.com>
2020-04-07 21:19:47 -0500

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,6 +8,66 @@ DEFAULT_PRINTER_URI = (
     f"ipp://{DEFAULT_PRINTER_HOST}:{DEFAULT_PRINTER_PORT}{DEFAULT_PRINTER_PATH}"
 )
 
+MOCK_IPP11_REQUIRED_ATTRIBUTES = {
+    "attributes-charset" "en",
+    "attributes-natural-language": "",
+    "charset-configured": "",
+    "charset-supported": "",
+    "compression-supported": "",
+    "document-format-default": "",
+    "document-format-supported": "",
+    "generated-natural-language-supported": "",
+    "ipp-versions-supported": "",
+    "operations-supported": "",
+    "pdl-override-supported": "",
+    "printer-is-accepting-jobs": "",
+    "printer-name": "",
+    "printer-state": "",
+    "printer-state-reasons": "",
+    "printer-up-time": "",
+    "printer-uri": "",
+    "printer-uri-supported": "",
+    "queued-job-count": "",
+    "request-id": "",
+    "requested-attributes": "",
+    "requesting-user-name": "",
+    "status-code": "",
+    "uri-authentication-supported": "",
+    "uri-security-supported": "",
+    "version-number": "",
+}
+
+MOCK_IPP20_ATTRIBUTES = MOCK_IPP11_REQUIRED_ATTRIBUTES + {
+    "color-supported": "",
+    "copies-default": "",
+    "copies-supported": "",
+    "finishings-default": "",
+    "finishings-supported": "",
+    "job-creation-attributes-supported": "",
+    "media-default": "",
+    "media-ready": "",
+    "media-supported": "",
+    "orientation-requested-default": "",
+    "orientation-requested-supported": "",
+    "output-bin-default": "",
+    "output-bin-supported": "",
+    "pages-per-minute": "",
+    "pages-per-minute-color": "",
+    "print-quality-default": "",
+    "print-quality-supported": "",
+    "printer-alert": "",
+    "printer-alert-description": "",
+    "printer-device-id": "",
+    "printer-info": "",
+    "printer-location": "",
+    "printer-make-and-model": "",
+    "printer-more-info": "",
+    "printer-resolution-default": "",
+    "sides-default": "",
+    "sides-supported": "",
+    "status-message": "",
+}
+
 
 def load_fixture(filename):
     """Load a fixture."""
