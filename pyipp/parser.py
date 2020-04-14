bd4562386799d6b4a4987fe4aa6f5f4ef75d4a8e
Chris Talkington <chris@talkingtontech.com>
2020-04-13 19:24:41 -0500

    Update parser.py

--- pyipp/parser.py
+++ pyipp/parser.py
@@ -3,7 +3,13 @@ import logging
 import struct
 from typing import Any, Dict, Tuple, cast
 
-from .enums import IppDocumentState, IppJobState, IppPrinterState, IppTag
+from .enums import (
+    IppDocumentState,
+    IppJobState,
+    IppPrinterState,
+    IppStatus,
+    IppTag,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
