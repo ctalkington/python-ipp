71bf456e1fe5c2018faf6fa7de759d010fa7a2f4
Chris Talkington <chris@talkingtontech.com>
2022-10-03 19:22:11 -0500

    update parser tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -107,3 +107,5 @@ def test_parse_brother_mfcj5320dw() -> None:
     printer = result["printers"][0]
     assert printer["printer-make-and-model"] == "Brother MFC-J5320DW"
     assert printer["printer-uuid"] == "urn:uuid:e3248000-80ce-11db-8000-30055ce13be2"
+
+    assert result["unsupported-attributes"] == []
